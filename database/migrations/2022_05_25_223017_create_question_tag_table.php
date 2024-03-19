cbe1aa9a565f4b1aead5a71b9a02dccce455f17f
amr hamdy <amrhamdy15@yahoo.com>
2024-03-19 05:18:26 +0200

    update  php  to   "8.2.4"

--- database/migrations/2022_05_25_223017_create_question_tag_table.php
+++ database/migrations/2022_05_25_223017_create_question_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQusetionTagTable extends Migration
+class CreateQuestionTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateQusetionTagTable extends Migration
      */
     public function up()
     {
-        Schema::create('qusetion_tag', function (Blueprint $table) {
+        Schema::create('question_tag', function (Blueprint $table) {
            $table->foreignId('question_id')->constrained('questions')->cascadeOnDelete();
             $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
             $table->primary(['question_id', 'tag_id']); //علشان ميتكررش حاجة فيهم خليتهم برايمرى
@@ -29,6 +29,6 @@ class CreateQusetionTagTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('qusetion_tag');
+        Schema::dropIfExists('question_tag');
     }
 }
