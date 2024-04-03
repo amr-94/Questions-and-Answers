0743e0a4990d9c9a2cada8ffa5353bc48f4ed2d9
amr hamdy <amrhamdy15@yahoo.com>
2024-04-03 03:14:00 +0200

    updates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -7,6 +7,7 @@ use App\Models\Tags;
 use App\Models\Answer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class QuestionsController extends Controller
 {
@@ -17,32 +18,32 @@ class QuestionsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except('index','show');
+        $this->middleware('auth')->except('index', 'show');
     }
 
- public function index()
+    public function index()
     {
         $search = request('search');
 
 
-        $questions= questions::with('user')
-        ->withCount('answers')
-        ->where('title', 'like', '%' . $search . '%')
-        ->orWhereHas('tags', function ($q) use ($search) {
+        $questions = questions::with('user')
+            ->withCount('answers')
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhereHas('tags', function ($q) use ($search) {
                 return $q->where('name', 'like', '%' . $search . '%');
             })
-        ->orWhereHas('user', function ($q) use ($search) {
+            ->orWhereHas('user', function ($q) use ($search) {
                 return $q->where('name', 'like', '%' . $search . '%');
             })
-        ->latest()
+            ->latest()
 
 
 
 
 
 
-        ->paginate(3);
-        return view('questions.index',[
+            ->paginate(3);
+        return view('questions.index', [
             'questions' => $questions,
         ]);
     }
@@ -52,11 +53,11 @@ class QuestionsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
- public function create()
+    public function create()
     {
         $tags = tags::all();
-        return view('questions.create',[
-            'tags'=> $tags
+        return view('questions.create', [
+            'tags' => $tags
         ]);
     }
 
@@ -66,18 +67,18 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-public function store(Request $request)
+    public function store(Request $request)
     {
 
         $request->validate([
-            'title' => ['required','string','max:255'],
-            'description'=>['required','string'],
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
             'img' => ['nullable', 'image'],
-            'tags'=>['required','array'],
+            'tags' => ['required', 'array'],
         ]);
 
 
-            $request->merge([
+        $request->merge([
 
             'user_id' => Auth::id(),
 
@@ -88,7 +89,7 @@ public function store(Request $request)
         $questions->tags()->attach($request->input('tags'));
 
         return redirect()->route('questions.index')
-        ->with('success','Question Add');
+            ->with('success', 'Question Add');
     }
 
     /**
@@ -97,16 +98,14 @@ public function store(Request $request)
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
- public function show($id)
+    public function show($id)
     {
-        $questions=  questions::findorfail($id);
-        $answer= $questions->answers()->latest()->with('user')->get();
-        return view('questions.show',[
+        $questions =  questions::findorfail($id);
+        $answer = $questions->answers()->latest()->with('user')->get();
+        return view('questions.show', [
             'question' => $questions,
-            'answer'=>$answer
+            'answer' => $answer
         ]);
-
-
     }
 
     /**
@@ -115,15 +114,15 @@ public function store(Request $request)
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
- public function edit($id)
+    public function edit($id)
     {
         $tags = tags::all();
         $questions =  questions::findorfail($id);
         $question_tag = $questions->tags()->pluck('id')->toArray();
         return view('questions.edit', [
             'question' => $questions,
-            'tags'=> $tags,
-            'question_tag'=> $question_tag,
+            'tags' => $tags,
+            'question_tag' => $question_tag,
 
         ]);
     }
@@ -135,33 +134,34 @@ public function store(Request $request)
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-public function update(Request $request, $id)
+    public function update(Request $request, $id)
     {
         $request->validate([
             'title' => ['required', 'string', 'max:255', "unique:questions,title,$id"],
-            'description' => ['required','string', "unique:questions,description,$id"],
-            'status' =>['in:open,close'],
+            'description' => ['required', 'string', "unique:questions,description,$id"],
+            'status' => ['in:open,close'],
             'img' => ['nullable', 'image'],
-            'tags' =>['required','array'],
+            'tags' => ['required', 'array'],
         ]);
 
-         $questions=questions::findorfail($id);
+        $questions = questions::findorfail($id);
+        $prev = $questions->img;
 
-          $prev = $questions->img;
-        if ($request->hasFile('q_img')) {
-            $file = $request->file('q_img');
-            $path = $file->store('/q_img');
+        if ($request->hasfile('q_img')) {
+            $fileName = time() . '.' . $request->q_img->extension();
+            $request->q_img->move(public_path('images/q_img'), $fileName);
+
             $request->merge([
-                'img' => $path,
+                'img' => $fileName
             ]);
         }
 
         $questions->update($request->all());
         $questions->tags()->sync($request->tags);
-           if($prev && $prev != $questions->img){
-            unlink('images/' .''. $prev);
+        if ($prev && $prev  !== $request->q_img) {
+            File::delete(public_path('images/q_img' . '/' . $prev));
         }
-         return redirect(route('questions.show', $questions->id))-> with('success', 'Question updated');
+        return redirect(route('questions.show', $questions->id))->with('success', 'Question updated');
     }
 
     /**
@@ -173,6 +173,6 @@ public function update(Request $request, $id)
     public function destroy($id)
     {
         questions::destroy($id);
-        return redirect(route('questions.index'))->with('success','Question Delete');
+        return redirect(route('questions.index'))->with('success', 'Question Delete');
     }
 }
